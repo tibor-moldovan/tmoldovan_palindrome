13479de81cfe578dbe6b4e644a49e2f8e0a1ef02
Tibor Moldovan <tibor.moldovan@gmail.com>
2019-06-04 21:15:41 -0500

    Finish working and refactored palindrome method

--- lib/tmoldovan_palindrome.rb
+++ lib/tmoldovan_palindrome.rb
@@ -1,6 +1,28 @@
 require "tmoldovan_palindrome/version"
 
-module TmoldovanPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
+
+
+
+
 end
+
+
+#module TmoldovanPalindrome
+#  class Error < StandardError; end
+  # Your code goes here...
+#end
--- test/tmoldovan_palindrome_test.rb
+++ test/tmoldovan_palindrome_test.rb
@@ -5,7 +5,24 @@ class TmoldovanPalindromeTest < Minitest::Test
     refute_nil ::TmoldovanPalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_non_palindrome
+	refute "apple".palindrome?
   end
+
+  def test_literal_palindrome
+  	assert "racecar".palindrome?
+  end
+
+ def test_mixed_case_palindrome
+  	assert "RaceCar".palindrome? 
+ end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  
+#def test_it_does_something_useful
+   # assert true
+  #end
 end
